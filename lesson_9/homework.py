efd4cf4e0462cfeb65d85285f14f57c9df82b16e
Bethlehemkahsay <134823108+Bethlehemkahsay@users.noreply.github.com>
2026-01-02 23:52:27 -0500

    Update homework.py

--- lesson_9/homework.py
+++ lesson_9/homework.py
@@ -6,13 +6,58 @@
 # Create an empty dictionary and then add a few of your friends. Make the key their email (can be fake)
 # and the value their name. When you're done, create the same dictionary as a pre-populated dictionary.
 
-# ----------------------------------------------------------------------------------------------------------------------
+### ANSWER ###
 
-# Nested Dictionary
+Friends = {}
+Friends["Beth@yahoo.com"]= "Beth"
+Friends["Solo@yahoo.com" ]= "Solae"
+Friends["Marry@yahoo.com"]= "Marry"
+
+## Prepopulated
+
+Friends = {
+    "Beth@yahoo.com", "Beth",
+    "Solo@yahoo.com" , "Solae",
+    "Marry@yahoo.com", "Marry"
+}
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+Nested Dictionary
 
 # Create a nested dictionary for a list of 5 company employees.
 # The key should be their employee id (an integer from 1-5 will do) and the value should be a dictionary with
 # the name, department and salary of the employee.
+
+### ANSWER ###
+
+employees = {
+    1: {
+        "name": "Beth",
+        "department": "Engineering",
+        "salary": 75000
+    },
+    2: {
+        "name": "Sol",
+        "department": "Marketing",
+        "salary": 62000
+    },
+    3: {
+        "name": "Dane",
+        "department": "Sales",
+        "salary": 58000
+    },
+    4: {
+        "name": "Diana",
+        "department": "HR",
+        "salary": 60000
+    },
+    5: {
+        "name": "Ethan",
+        "department": "Finance",
+        "salary": 68000
+    }
+}
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -23,6 +68,15 @@
 # - Print a list of the employee IDs
 # - Print the employee data for employee with the ID 3.
 # - Loop over the employees and print all their names and salaries.
+
+### ANSWER ###   
+
+print(employees.keys())
+
+print(employees[3])
+
+for emp_id, info in employees.items():
+    print(info["name"], info["salary"])
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -40,12 +94,53 @@ salaries = {
 # We need to increase everyone's salary by 1,000 and also add a new employee joseph with a salary of 18,000.
 # Please come up with a way to do this using update()
 
+### ANSWER ###
+
+salaries = {
+    'james': 10000,
+    'tom': 15000,
+    'ryan': 16000,
+    'julia': 17000
+}
+
+updated_salaries = {}
+
+for name, salary in salaries.items():
+    updated_salaries[name] = salary + 1000
+
+salaries.update(updated_salaries)
+salaries.update({'joseph': 18000})
+
+print(salaries)
+
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Deleting Values
 
 # You remember those employees from Updating Values section? Well, Julia got fired, so we need to remove her
 # name from the salaries dictionary. How would you do that?
+
+### ANSWER ###
+
+salaries = {
+    'james': 10000,
+    'tom': 15000,
+    'ryan': 16000,
+    'julia': 17000
+}
+
+updated_salaries = {}
+
+for name, salary in salaries.items():
+    updated_salaries[name] = salary + 1000
+
+salaries.update(updated_salaries)
+salaries.update({'joseph': 18000})
+
+
+del salaries['julia']
+print(salaries)
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -66,6 +161,31 @@ films = {
    2019: "Avengers: Endgame",
    2020: "Bad Boys for Life"
 }
+
+### ANSWER ###
+
+# - Is Black Panther in the list of movies?
+
+if "Black Panther" in films.values():
+    print("Black Panther is in the list of movies.")
+else:
+    print("Black Panther is NOT in the list of movies.")
+
+# - Is there any movie for the year 2021?
+
+if 2021 in films.keys():
+    print("There is a movie for the year 2021.")
+else:
+    print("There is NO movie for the year 2021.")
+
+# - Print a message for each element that shows the year, the title and the position in the dictionary (1-5).
+
+counter = 1
+
+for year, title in films.items():
+    print(f"{counter}. Year: {year}, Title: {title}")
+    counter += 1
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -101,6 +221,35 @@ films = {
 # for ???, ??? in shelter_pets.i???():
 # if not info['???']:
 # print(pet)
+
+### ANSWER ###
+
+shelter_pets = {
+    'Whiskers': {'Age': 2, 'Type': 'Cat', 'Adopted': False},
+    'Fido': {'Age': 4, 'Type': 'Dog', 'Adopted': True},
+    'Patch': {'Age': 1, 'Type': 'Dog', 'Adopted': False},
+    'Snowball': {'Age': 3, 'Type': 'Rabbit', 'Adopted': True}
+}
+# Access and print the age of Whiskers
+# print(shelter_pets['Whiskers']['Age'])  # Should output 2
+print(shelter_pets['Whiskers']['Age'])
+
+# Access and print if Patch is a dog or cat
+# print(???)
+print(shelter_pets['Patch']['Type'])
+
+# Access and print if Snowball is adopted or not
+# print(???)
+print(shelter_pets['Snowball']['Adopted'])
+
+# Find out which pets are not yet adopted and print their names
+# for ???, ??? in shelter_pets.i???():
+# if not info['???']:
+# print(pet)
+for pet, info in shelter_pets.items():
+    if not info['Adopted']:
+        print(pet)
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -144,6 +293,25 @@ films = {
 
 # Print the updated dictionary of best selling books.
 
+### ANSWER ###
+
+best_selling_books = {
+    1997: "Harry Potter and the Philosopher's Stone",
+    1984: "Neuromancer",
+    2003: "The Kite Runner",
+    2015: "Go Set a Watchman"
+}
+best_selling_books[1997] = "Harry Potter and the Sorcerer's Stone"
+
+best_selling_books.update({
+    1984: "The Hunt for Red October",
+    2003: "The Da Vinci Code"
+})
+
+del best_selling_books[2015]
+
+print(best_selling_books)
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Exercise 3. Manage Music Collection
@@ -165,7 +333,7 @@ films = {
 #   1975: "Blood on the Tracks",
 #   1997: "Time Out of Mind"
 # }
-
+ 
 # Use .keys() to loop through and print out all the release years
 # for year in dylan_albums.keys():
 # print(year)
@@ -178,6 +346,10 @@ films = {
 # Remember the keyword by default checks only the keys, not the values.
 # If you want to check if a particular value (in this case, an album name),
 # you need to specify that you're searching within the dictionary's values.
+
+### ANSWER ##3
+
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -197,6 +369,25 @@ films = {
 # - Create an empty dictionary result = {}
 # - Make a for loop to iterate over person dictionary
 # - If item’s value not in result dict, add key value part into result.
+
+### ANSWER ###
+
+person = {
+    'first': 'Jeff',
+    'name': 'Jeff',
+    'last': 'Smith',
+    'last_name': 'Smith',
+    'state': 'CA',
+    'age': 55
+}
+result = {}
+
+for key, value in person.items():
+    if value not in result.values():
+        result[key] = value
+
+    print(result)
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -227,3 +418,42 @@ films = {
 # - Check if the current score (v) is >= to the current maximum score
 # - If so, update the max score and assign the key-value pair to the result
 # - Return result and test the function
+
+### ANSWER ###
+
+test_scores = {
+    'James': 83,
+    'Julia': 91,
+    'Ryan': 90,
+    'Maria': 80,
+    'David': 79,
+    'Adam': 96,
+    'Jennifer': 97,
+    'Susan': 77
+}
+
+def find_max_score(scores_dict):
+    result = {}
+    max_score = 0
+
+    for name, score in scores_dict.items():
+        if score >= max_score:
+            max_score = score
+            result = {name: score}
+
+    return result
+
+def find_max_score(scores_dict):
+    result = {}
+    max_score = 0
+
+    for name, score in scores_dict.items():
+        if score >= max_score:
+            max_score = score
+            result = {name: score}
+
+    return result
+
+highest_score = find_max_score(test_scores)
+print(highest_score)
+
